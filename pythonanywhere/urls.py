ebc7d7a42f0e5c8025f1a3dbf8d7d1ee8a31facc
Allegromare <allegromare@gmail.com>
2023-07-05 21:30:10 +0200

    Sistemazione url homepage

--- pythonanywhere/urls.py
+++ pythonanywhere/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("website/", include("website.urls")),
-    path("", views.index, name="index"),
+    path("admin/", admin.site.urls),
+    path("", include("website.urls")),
 ]
